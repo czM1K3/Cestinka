d5053544ea63cf69df49e653fd465ed16fcf6cf8
czM1K3 <misamadera1@gmail.com>
2021-09-13 11:21:21 +0200

    Prettier

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -5,35 +5,38 @@ import Link from "next/link";
 
 type ZapisekProps = {
 	postData: PostType;
-}
+};
 
 const Zapisek: FC<ZapisekProps> = ({ postData }) => {
 	return (
 		<div>
-            <Link href="/">Zpět</Link>
+			<Link href="/">Zpět</Link>
 			<h1>{postData.title}</h1>
-			<div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
-
+			<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
 		</div>
 	);
-}
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const paths = getAllPostsIds();
-    return {
-        paths,
-        fallback: false,
-    };
-}
+	const paths = getAllPostsIds();
+	return {
+		paths,
+		fallback: false,
+	};
+};
 
-export const getStaticProps = async ({params}: {params: { id: string } }) => {
+export const getStaticProps = async ({
+	params,
+}: {
+	params: { id: string };
+}) => {
 	if (!params) return null;
-    const postData = await getPostData(params.id as string);
-    return {
-        props: {
-            postData,
-        },
-    };
-}
+	const postData = await getPostData(params.id as string);
+	return {
+		props: {
+			postData,
+		},
+	};
+};
 
 export default Zapisek;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
+	return <Component {...pageProps} />;
+};
 export default MyApp;
