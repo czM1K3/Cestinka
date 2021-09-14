00e2d33e15c4f900aa94e102da6865a49d8027cc
Josef Kahoun <pepa.kahy@gmail.com>
2021-09-14 10:38:04 +0200

    Added NextUI

--- Components/NoLogin/index.tsx
+++ Components/NoLogin/index.tsx
@@ -1,11 +1,14 @@
 import { signIn } from "next-auth/client";
 import React, { FC } from "react";
+import { Button } from '@nextui-org/react';
+import { Container } from '@nextui-org/react';
+import styles from "../../styles/zapisek.module.scss";
 
 const NoLogin: FC = () => {
 	return (
-		<div>
+		<div className={styles.center}>
 			Musíte se přihlásit.<br />
-			<button onClick={() => signIn()}>Přihlásit se</button>
+			<Button auto onClick={() => signIn()}>Přihlásit se</Button>
 		</div>
 	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,12 @@ import type { GetStaticProps } from "next";
 import Link from "next/link";
 import Head from "next/head";
 import { getPostsForSort, PostsForSort } from "../lib/posts";
-import styles from "../styles/Home.module.css";
 import { signOut } from "next-auth/client"
 import Layout from "../Components/Layout";
+import { Button } from '@nextui-org/react';
+import { Text } from '@nextui-org/react';
+import { Container, Row, Col } from '@nextui-org/react';
+import styles from "../styles/zapisek.module.scss";
 
 type HomeProps = {
 	allPostsData: PostsForSort[];
@@ -14,26 +17,30 @@ type HomeProps = {
 const Home: FC<HomeProps> = ({ allPostsData }) => {
 	const [search, setSearch] = useState("");
 	return (
+		<div className={styles.cont}>
 		<Layout>
-			<button onClick={() => signOut()}>Sign Out</button>
-			<div className={styles.container}>
-				<main className={styles.main}>
-					<h1 className={styles.title}>Vítejte v zápiscích!</h1>
+			<Container fluid>
+			<Button shadow auto onClick={() => signOut()}>Odhlásit se</Button>
+			<div>
+				<main>
+					<h1>Vítejte v zápiscích!</h1>
 
 					<form>
 						<input type="text" onChange={(e) => setSearch(e.target.value.toLocaleLowerCase())} />
 					</form>
 
-					<ul>
+					<ul className={styles.ul}>
 						{allPostsData.filter(x => x.content.includes(search)).map((item) => (
 							<li key={item.id}>
-								<Link href={`/${item.id}${search ? `?hledat=${search}` : ""}`}>{item.title}</Link>
+								<Text color="primary"><Link href={`/${item.id}${search ? `?hledat=${search}` : ""}`}>{item.title}</Link></Text>
 							</li>
 						))}
 					</ul>
 				</main>
 			</div>
+			</Container>
 		</Layout>
+		</div>
 	);
 };
 
