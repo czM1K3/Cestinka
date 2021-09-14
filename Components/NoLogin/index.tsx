77cea7b017657b84bc48d08d92d82aa725c87450
Josef Kahoun <pepa.kahy@gmail.com>
2021-09-14 10:46:16 +0200

    Updated styles

--- Components/NoLogin/index.tsx
+++ Components/NoLogin/index.tsx
@@ -1,14 +1,13 @@
 import { signIn } from "next-auth/client";
 import React, { FC } from "react";
-import { Button } from '@nextui-org/react';
-import { Container } from '@nextui-org/react';
 import styles from "../../styles/zapisek.module.scss";
+import { Button } from '@nextui-org/react';
 
 const NoLogin: FC = () => {
 	return (
 		<div className={styles.center}>
-			Musíte se přihlásit.<br />
-			<Button auto onClick={() => signIn()}>Přihlásit se</Button>
+			<h1>Musíte se přihlásit.</h1>
+			<Button auto shadow onClick={() => signIn()}>Přihlásit se</Button>
 		</div>
 	);
 }
