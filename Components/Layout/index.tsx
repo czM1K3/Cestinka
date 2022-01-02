060f7e3450373638351112e9101203c8b1602ad2
czM1K3 <misamadera1@gmail.com>
2022-01-02 14:25:06 +0100

    Changed name

--- Components/Layout/index.tsx
+++ Components/Layout/index.tsx
@@ -29,7 +29,7 @@ const Layout: FC = ({ children }) => {
 					sizes="32x32"
 				/>
 				<link rel="apple-touch-icon" href="/apple-icon.png"></link>
-				<meta name="theme-color" content="#317EFB" />
+				<meta name="theme-color" content="#0d6cf9" />
 			</Head>
 			<main className="main">
 				{children}
